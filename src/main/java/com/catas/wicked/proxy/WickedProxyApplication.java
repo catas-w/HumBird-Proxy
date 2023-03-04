c86e3612f9705935fd5e933d0518a50035bbbcd9
catas <1014518525@qq.com>
2023-03-05 00:51:03 +0800

    gui develop

--- src/main/java/com/catas/wicked/proxy/WickedProxyApplication.java
+++ src/main/java/com/catas/wicked/proxy/WickedProxyApplication.java
@@ -3,10 +3,15 @@ package com.catas.wicked.proxy;
 import com.catas.wicked.proxy.gui.ApplicationView;
 import com.catas.wicked.proxy.gui.CustomLoadingView;
 import de.felixroske.jfxsupport.AbstractJavaFxApplicationSupport;
+import javafx.application.ConditionalFeature;
+import javafx.application.Platform;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Collection;
 
 @SpringBootApplication
 public class WickedProxyApplication extends AbstractJavaFxApplicationSupport {
@@ -16,9 +21,14 @@ public class WickedProxyApplication extends AbstractJavaFxApplicationSupport {
     }
 
 
-    // @Override
-    // public void beforeInitialView(Stage stage, ConfigurableApplicationContext ctx) {
-    //     stage.initStyle(StageStyle.UNDECORATED);
-    // }
+    @Override
+    public void beforeInitialView(Stage stage, ConfigurableApplicationContext ctx) {
+        stage.initStyle(StageStyle.DECORATED);
+    }
+
+    @Override
+    public Collection<Image> loadDefaultIcons() {
+        return super.loadDefaultIcons();
+    }
 
 }
--- src/main/java/com/catas/wicked/proxy/gui/AppController.java
+++ src/main/java/com/catas/wicked/proxy/gui/AppController.java
@@ -77,11 +77,11 @@ public class AppController implements Initializable {
                 ":method: GET\n" +
                 ":path: /questions/87133\n" +
                 ":scheme: https\n" +
-                "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7\n" +
+                "accept: text/html,application/xhtml+xml,application/xml;q=0.9d-exchange;v=b3;q=0.7\n" +
                 "accept-encoding: gzip, deflate, br\n" +
                 "accept-language: zh-CN,zh;q=0.9,en;q=0.8\n" +
                 "cache-control: max-age=0\n" +
-                "cookie: user_device_id=22a5278e40a046558d230b95c023bc4b; user_device_id_timestamp=1676912173861; cf_zaraz_google-analytics_2906=true; google-analytics_2906___ga=ee40da82-db43-421c-b639-7b8db5fd756b\n" +
+                "cookie: user_device_id=22a5278e40a046558d230b95c023bc4b; user_device_id_timestamp=16769156b\n" +
                 "dnt: 1\n" +
                 "referer: https://www.bing.com/\n" +
                 "sec-ch-ua: \"Chromium\";v=\"110\", \"Not A(Brand\";v=\"24\", \"Google Chrome\";v=\"110\"\n" +
@@ -92,7 +92,8 @@ public class AppController implements Initializable {
                 "sec-fetch-site: cross-site\n" +
                 "sec-fetch-user: ?1\n" +
                 "upgrade-insecure-requests: 1\n" +
-                "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36");
+                "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) " +
+                "Chrome/110.0.0.0 Safari/537.36");
         reqHeaderText.setEditable(false);
 
         addTitleListener(reqHeaderPane);
@@ -111,12 +112,12 @@ public class AppController implements Initializable {
         bindProxySettingBtn();
 
         // close btn
-        closeBtn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                Platform.exit();
-            }
-        });
+        // closeBtn.setOnAction(new EventHandler<ActionEvent>() {
+        //     @Override
+        //     public void handle(ActionEvent actionEvent) {
+        //         Platform.exit();
+        //     }
+        // });
     }
 
     private void bindProxySettingBtn() {
